4473069916af85a5fa36cc5544148f484cbc0d99
camilotmundim <camilomundim@gmail.com>
2024-07-15 14:35:39 -0400

    criando app

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse("Hello World!")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', my_view),
 ]
