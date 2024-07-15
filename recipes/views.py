d28d6e239ee220d9a89d779b31aa051473e9ef67
camilomundim <camilomundim@tjro.jus.br>
2024-07-15 19:28:02 -0400

    Criando views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+
+def sobre(request):
+    return HttpResponse("Sobre")
