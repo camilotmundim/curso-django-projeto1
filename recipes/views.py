6595e67b6eb23571d623afd04572854fee03cb64
camilomundim <camilomundim@tjro.jus.br>
2025-02-17 11:03:29 -0400

    feat: criando pagination

--- recipes/views.py
+++ recipes/views.py
@@ -3,19 +3,19 @@ from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
+from utils.pagination import make_pagination
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by("-id")
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
     return render(
         request,
         "recipes/pages/home.html",
-        context={
-            "recipes": recipes,
-        },
+        context={"recipes": page_obj, "pagination_range": pagination_range},
     )
 
 
@@ -26,12 +26,14 @@ def category(request, category_id):
             is_published=True,
         ).order_by("-id")
     )
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
     return render(
         request,
         "recipes/pages/category.html",
         context={
-            "recipes": recipes,
+            "recipes": page_obj,
+            "pagination_range": pagination_range,
             "title": f"{recipes[0].category.name} - Category | ",
         },
     )
@@ -67,12 +69,16 @@ def search(request):
         is_published=True,
     ).order_by("-id")
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(
         request,
         "recipes/pages/search.html",
         {
             "page_title": f'Search for "{search_term}" |',
             "search_term": search_term,
-            "recipes": recipes,
+            "recipes": page_obj,
+            "pagination_range": pagination_range,
+            "additional_url_query": f"&q={search_term}",
         },
     )
