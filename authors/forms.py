769ffd96da6b269f02d73fb5966e094261dd523a
camilomundim <camilomundim@tjro.jus.br>
2025-07-14 07:47:25 -0400

    feat: salvando o forumlário de usuário

--- authors/forms.py
+++ authors/forms.py
@@ -1,9 +1,31 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, "")
+    field.widget.attrs[attr_name] = f"{existing} {attr_new_val}".strip()
 
 
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs["placeholder"] = placeholder_val
+    add_attr(field, "placeholder", placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$")
+
+    if not regex.match(password):
+        raise ValidationError(
+            (
+                "Password must have at least one uppercase letter, "
+                "one lowercase letter and one number. The length should be "
+                "at least 8 characters."
+            ),
+            code="invalid",
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -13,6 +35,7 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields["email"], "Your e-mail")
         add_placeholder(self.fields["first_name"], "Ex.: John")
         add_placeholder(self.fields["last_name"], "Ex.: Doe")
+        add_attr(self.fields["username"], "css", "a-css-class")
 
     password = forms.CharField(
         required=True,
@@ -23,6 +46,7 @@ class RegisterForm(forms.ModelForm):
             "one lowercase letter and one number. The length should be "
             "at least 8 characters."
         ),
+        validators=[strong_password],
     )
     password2 = forms.CharField(
         required=True,
@@ -66,3 +90,46 @@ class RegisterForm(forms.ModelForm):
                 attrs={"placeholder": "Type your password here"}
             ),
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get("password")
+
+        if "atenção" in data:
+            raise ValidationError(
+                "Não digite %(pipoca)s no campo password",
+                code="invalid",
+                params={"pipoca": '"atenção"'},
+            )
+
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get("first_name")
+
+        if "John Doe" in data:
+            raise ValidationError(
+                "Não digite %(value)s no campo first name",
+                code="invalid",
+                params={"value": '"John Doe"'},
+            )
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                "Password and password2 must be equal", code="invalid"
+            )
+            raise ValidationError(
+                {
+                    "password": password_confirmation_error,
+                    "password2": [
+                        password_confirmation_error,
+                    ],
+                }
+            )
